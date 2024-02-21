b66686ce8fa74cad6e6dfee92afd77e65f5fa04a
Niks2291 <niksedition@gmail.com>
2024-02-21 20:12:01 +0530

    Added LoginForm

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from "./Controller/Layout/Layout";
 import CreateCustomerForm from './Controller/Admin/CreateCustomerForm';
+import LoginForm from './Components/LoginForm/LoginForm';
 
 function Content() {
   return (
@@ -20,9 +21,13 @@ function App() {
   
   return (
     <div>
+      <LoginForm/>
       <WrappedContent />
     </div>
+    
   );
 }
+
+
 
 export default App;
