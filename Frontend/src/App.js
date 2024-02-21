1b8dfc1d1bf0322963b284ea4b31086a71c542bc
Satish Dattatray Bobalade <satishbobalade2002@gmail.com>
2024-02-21 14:37:00 +0530

    initial

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,7 +9,8 @@ function App() {
         className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         href="#"
       >
-        Get Started
+        Get Started hello bhai
+      
       </a>
     </div>
   </div>
