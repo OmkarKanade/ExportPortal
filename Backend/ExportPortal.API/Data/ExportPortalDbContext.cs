1072040584f6174d47b773d477b73b90248b21ee
OmkarKanade <omkarkanade8888@gmail.com>
2024-02-21 13:37:31 +0530

    Initial Commit

---
+++ Backend/ExportPortal.API/Data/ExportPortalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExportPortal.API.Data
+{
+    public class ExportPortalDbContext: IdentityDbContext
+    {
+        public ExportPortalDbContext(DbContextOptions<ExportPortalDbContext> dbContextOptions): base(dbContextOptions)
+        {
+
+        }
+
+    }
+}
---
+++ Frontend/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome to Export Portal
+      </h1>
+      <a
+        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+        href="#"
+      >
+        Get Started
+      </a>
+    </div>
+  </div>
+  );
+}
+
+export default App;
