16d0cd8ae04ff66b95bce99bbab89b95c8abb5e3
Niks2291 <niksedition@gmail.com>
2024-02-22 16:01:34 +0530

    hhh

--- Frontend/src/Controller/Customer/CustomerDashboard.js
+++ Frontend/src/Controller/Customer/CustomerDashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../Layout/Header';
+import Header from './CustomerHeader';
 import CustomerSidebar from '../Layout/CustomerSidebar';
 
 
--- Frontend/src/Controller/Customer/CustomerHeader.js
+++ Frontend/src/Controller/Customer/CustomerHeader.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./header.css";
+import "./customerheader.css";
 
 export default function Header() {
   return (
     <header className="Header">
-      <h1><a href="#">Admin</a></h1>
+      <h1><a href="#">User Login</a></h1>
       <div className="HeaderButtons">
         <button className="HeaderButton">My Cart</button>
         <button className="HeaderButton">Logout</button>
