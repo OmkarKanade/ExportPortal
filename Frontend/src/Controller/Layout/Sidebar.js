4b051a3a43d205e292d8361e3dd3f43151bddaf1
Animish2002 <animishchopade123@gmail.com>
2024-02-23 20:31:22 +0530

    wrong changes

--- Frontend/src/Controller/Layout/Sidebar.js
+++ Frontend/src/Controller/Layout/Sidebar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './sidebar.css';
 
 const Sidebar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const lists = [
     { name: 'Create Customer Account', path: '/create-customerForm' },
     { name: 'Create Vendor Account', path: '/create-vendorForm' },
@@ -11,12 +17,19 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className='app-container'>
+    <div className={`app-container ${menuOpen ? 'menu-open' : ''}`}>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? 'bar1-open' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'bar2-open' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'bar3-open' : ''}`}></div>
+      </div>
       <div className="sidebar">
         <ul>
           {lists.map((list, index) => (
             <li key={index}>
-              <Link to={list.path}>{list.name}</Link>
+              <Link to={list.path} onClick={() => setMenuOpen(false)}>
+                {list.name}
+              </Link>
             </li>
           ))}
         </ul>
