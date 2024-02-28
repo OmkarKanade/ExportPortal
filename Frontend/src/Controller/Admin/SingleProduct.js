4e1e6752a636de0448c84a3c88b2946a035f9ab3
LokeshDC16 <lokeshdc2002@gmail.com>
2024-02-28 14:55:32 +0530

    sdsd

--- Frontend/src/Controller/Admin/SingleProduct.js
+++ Frontend/src/Controller/Admin/SingleProduct.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 const SingleProduct = ({ productId }) => {
   const [product, setProduct] = useState(null);
 
