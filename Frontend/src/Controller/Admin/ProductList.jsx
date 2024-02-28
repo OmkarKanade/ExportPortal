c54cec723411cb3b3729d9af89e875d7716c3517
LokeshDC16 <lokeshdc2002@gmail.com>
2024-02-28 15:16:34 +0530

    asas

--- Frontend/src/Controller/Admin/ProductList.jsx
+++ Frontend/src/Controller/Admin/ProductList.jsx
@@ -4,23 +4,19 @@ import Layout from '../Layout/Layout';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-
-const ProductCard = ({ id, name, scientificName, totalRate, grossWeight, ingredients, certificationid }) => {
-  return (
-    <div className="carddd" >
-      <div className="carddd-info">
-        <h5 className="carddd-text">Product ID: {id}</h5>
-        <h5 className="carddd-text">Name: {name}</h5>
-        <p className="carddd-text">Scientific Name: {scientificName}</p>
-        <p className="carddd-text">Total Price: {totalRate} Rs</p>
-        <p className="carddd-text">Gross Weight: {grossWeight} g</p>
-        <p className="carddd-text">Ingredients: {ingredients}</p>
-        <p className="carddd-text">Certification ID: {certificationid}</p>
-      </div>
+const ProductCard = ({ id, name, scientificName, totalRate, grossWeight, ingredients, certificationId }) => (
+  <div className="carddd">
+    <div className="carddd-info">
+      <h5 className="carddd-text">Product ID: {id}</h5>
+      <h5 className="carddd-text">Name: {name}</h5>
+      <p className="carddd-text">Scientific Name: {scientificName}</p>
+      <p className="carddd-text">Total Price: {totalRate} Rs</p>
+      <p className="carddd-text">Gross Weight: {grossWeight} g</p>
+      <p className="carddd-text">Ingredients: {ingredients}</p>
+      <p className="carddd-text">Certification ID: {certificationId}</p>
     </div>
-  );
-};
-
+  </div>
+);
 
 const ProductPage = () => {
   const [products, setProducts] = useState([]);
@@ -30,7 +26,6 @@ const ProductPage = () => {
       try {
         const response = await axios.get('https://localhost:7051/api/Product');
         setProducts(response.data);
-        console.log(ProductCard);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -38,29 +33,7 @@ const ProductPage = () => {
 
     fetchData();
   }, []);
-  // const products = [
-  //   {
-  //     id: 1,
-  //     name: 'Parle G Biscuits',
-  //     price: 10,
-  //     image: '/images/parleg.jpeg',
-  //     description: 'Weight: 20gm',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Good Day Biscuits',
-  //     price: 25,
-  //     image: '/images/GoodDay.png',
-  //     description: 'Weight: 25g',
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Krack Jack Biscuits',
-  //     price: 35,
-  //     image: '/images/KrackJack.jpeg',
-  //     description: 'Weight: 35g',
-  //   },
-  // ];
+
   return (
     <Layout>
       <div className="product-page">
@@ -80,8 +53,8 @@ const ProductPage = () => {
           ))}
         </div>
       </div>
-    </Layout>
-  );
+    </Layout>
+  );
 };
 
 export default ProductPage;
