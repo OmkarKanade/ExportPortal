93dd6e3be628ffff09d92a1f5161e5b1e29c8ec2
Animish2002 <animishchopade123@gmail.com>
2024-03-07 14:05:02 +0530

    Reapply "Updated for Prod_Num field in Product Model"
    This reverts commit 45ef05175b1de3f82916b8fea7a864b81f0915f6.

--- Backend/ExportPortal.API/Models/DTO/ProductResponseDTO.cs
+++ Backend/ExportPortal.API/Models/DTO/ProductResponseDTO.cs
@@ -4,8 +4,8 @@ namespace ExportPortal.API.Models.DTO
 {
     public class ProductResponseDTO
     {
-        public string Id { get; set; } // Change data type to string
-
+        public Guid Id { get; set; }
+        public string ProductId { get; set; }
         public string Name { get; set; }
         public string ImgPath { get; set; }
         public string ScientificName { get; set; }
