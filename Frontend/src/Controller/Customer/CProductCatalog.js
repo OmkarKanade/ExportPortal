ea96b8915c3b19ed8feabb7c75b272e4845a3e10
Animish2002 <animishchopade123@gmail.com>
2024-03-09 12:02:31 +0530

    Refactor product catalog page. Remove search, filter, and sort features.

--- Frontend/src/Controller/Customer/CProductCatalog.js
+++ Frontend/src/Controller/Customer/CProductCatalog.js
@@ -6,11 +6,6 @@ import "react-toastify/dist/ReactToastify.css";
 
 const CProductCatalog = () => {
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filterBy, setFilterBy] = useState("name");
-  const [filterValue, setFilterValue] = useState("");
-  const [sortOrder, setSortOrder] = useState("asc"); // Default sorting order
   const [customerId, setCustomerId] = useState("");
 
   useEffect(() => {
@@ -18,112 +13,39 @@ const CProductCatalog = () => {
       try {
         const response = await axios.get("https://localhost:7051/api/Product");
         setProducts(response.data);
-        setFilteredProducts(response.data); // Initialize filtered products with all products
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-    // Fetch customerId from sessionStorage
+
     const sid = sessionStorage.getItem("sid");
-    setCustomerId(sid);
+    setCustomerId(sid || ""); // Set default value to empty string if sid is null
   }, []);
 
-  // Function to handle search input change
-  const handleSearchChange = (event) => {
-    const searchTerm = event.target.value;
-    setSearchTerm(searchTerm);
-
-    applyFilters(searchTerm, filterBy, filterValue);
-  };
-
-  // Function to handle filter by change
-  const handleFilterByChange = (event) => {
-    const selectedFilter = event.target.value;
-    setFilterBy(selectedFilter);
-
-    applyFilters(searchTerm, selectedFilter, filterValue);
-  };
-
-  // Function to handle filter value change
-  const handleFilterValueChange = (event) => {
-    const value = event.target.value;
-    setFilterValue(value);
-
-    applyFilters(searchTerm, filterBy, value);
-  };
-
-  // Function to handle sort order change
-  const handleSortOrderChange = (event) => {
-    const selectedOrder = event.target.value;
-    setSortOrder(selectedOrder);
-
-    sortProducts(selectedOrder);
-  };
-
-  // Function to apply filters
-  const applyFilters = (searchTerm, filterBy, value) => {
-    let filtered = products;
-
-    // Filter by search term
-    filtered = filtered.filter((product) =>
-      product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    // Filter by selected filter
-    if (value !== "") {
-      filtered = filtered.filter((product) =>
-        String(product[filterBy]).toLowerCase().includes(value.toLowerCase())
-      );
-    }
-
-    // Apply sorting
-    sortProducts(sortOrder, filtered);
-  };
-
-  // Function to sort products
-  const sortProducts = (order, productsList = filteredProducts) => {
-    const sortedProducts = [...productsList];
-
-    sortedProducts.sort((a, b) => {
-      const rateA = parseFloat(a.totalRate);
-      const rateB = parseFloat(b.totalRate);
-
-      if (order === "asc") {
-        return rateA - rateB;
-      } else {
-        return rateB - rateA;
-      }
-    });
-
-    setFilteredProducts(sortedProducts);
-  };
-
-  // Function to handle quantity change and calculate total quantity
   const handleQuantityChange = (productId, newQuantity) => {
-    const updatedProducts = filteredProducts.map((product) => {
+    const updatedProducts = products.map((product) => {
       if (product.id === productId) {
         const totalQuantity = newQuantity * product.bagOrBoxBumpers;
         return {
           ...product,
-          quantity: newQuantity,
+          netquantity: newQuantity,
           totalQuantity: totalQuantity,
         };
       }
       return product;
     });
-    setFilteredProducts(updatedProducts);
+    setProducts(updatedProducts);
   };
 
-  // Function to post quotation
-  const postQuotation = async (productId, totalQuantity, customerId) => {
+  const postQuotation = async (productId, quantity, customerId) => {
     try {
       const response = await axios.post(
         "https://localhost:7051/api/Quotation",
         {
           productId: productId,
-          totalQuantity: totalQuantity,
+          totalQuantity: quantity,
           customerId: customerId,
         }
       );
@@ -142,47 +64,10 @@ const CProductCatalog = () => {
     <Fragment>
       <CustomerDashboard>
         <ToastContainer />
-        {/* Search Input, Filter Options, and Sorting */}
+        {/* Product Table */}
         <h1 className="text-3xl text-gray-700 font-bold mb-4">
           Products Catalog
         </h1>
-        <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-4">
-          {/* Search Input */}
-          <div className="flex items-center">
-            {/* Filter By Dropdown */}
-            <select
-              value={filterBy}
-              onChange={handleFilterByChange}
-              className="border border-gray-300 px-3 py-2 rounded-md mr-2"
-            >
-              <option value="name">Name</option>
-              <option value="hsnCode">HSN Code</option>
-              <option value="totalRate">Total Price</option>
-              {/* Add more options for other filters */}
-            </select>
-
-            {/* Filter Value Input */}
-            <input
-              type="text"
-              placeholder={`Filter by ${filterBy}`}
-              value={filterValue}
-              onChange={handleFilterValueChange}
-              className="border border-gray-300 px-3 py-2 rounded-md mr-2"
-            />
-
-            {/* Sort Order Dropdown */}
-            <select
-              value={sortOrder}
-              onChange={handleSortOrderChange}
-              className="border border-gray-300 px-3 py-2 rounded-md"
-            >
-              <option value="asc">Price Low to High</option>
-              <option value="desc">Price High to Low</option>
-            </select>
-          </div>
-        </div>
-
-        {/* Product Table */}
         <div className="overflow-x-auto">
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-sky-700">
@@ -206,10 +91,10 @@ const CProductCatalog = () => {
                   Bag or Box
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                  Quantity
+                  Net Quantity
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                  Total Quantity
+                  Quantity
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
                   Action
@@ -217,8 +102,8 @@ const CProductCatalog = () => {
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
-              {filteredProducts && filteredProducts.length > 0 ? (
-                filteredProducts.map((product, index) => {
+              {products && products.length > 0 ? (
+                products.map((product, index) => {
                   return (
                     <tr key={product.id}>
                       <td className="px-6 py-4 whitespace-nowrap">
@@ -244,7 +129,7 @@ const CProductCatalog = () => {
                           <input
                             type="number"
                             min="0"
-                            value={product.quantity || 0}
+                            value={product.netquantity || 0}
                             onChange={(e) =>
                               handleQuantityChange(
                                 product.id,
@@ -264,8 +149,8 @@ const CProductCatalog = () => {
                           onClick={() =>
                             postQuotation(
                               product.id,
-                              product.totalQuantity,
-                              customerId // Replace 'customerId' with the actual ID
+                              product.netquantity,
+                              customerId
                             )
                           }
                         >
